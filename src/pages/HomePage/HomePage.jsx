17cfa1efe6a69ed0d775ced87e270d59fb425625
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-12-17 00:35:39 +0200

    Add HomePage markup

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,8 +4,32 @@ const HomePage = () => {
   return (
     <main>
       <section>
-        <h1>Welcome to Our Store!</h1>
-        <p>Explore our wide range of organic products and berries!</p>
+        <h1>
+          fresh harvest box is your one-stop place for a delicious fruit basket
+        </h1>
+        <p>
+          Our expertly curated fruit baskets are made with the freshest, highest
+          quality fruits available. Whether you are looking for a healthy snack
+          or a gift for a loved one, Fresh Harvest Box has got you covered.
+        </p>
+        <img src="/src/img/image1.png" alt="" />
+        <p>Fruit</p>
+        <p>
+          Refreshing and juicy, watermelon is the perfect summer treat and a
+          great source of hydration
+        </p>
+        <img src="/src/img/image2.png" alt="" />
+        <p>Fruit</p>
+        <p>
+          Sweet and juicy, strawberries are packed with vitamin C and
+          antioxidants, making them a delicious and healthy snack
+        </p>
+        <img src="/src/img/image3.png" alt="" />
+        <p>#organic</p>
+        <p>#products</p>
+        <p>#basket</p>
+        <p>#fruits</p>
+        <button type="button">Shop now</button>
         <FeaturedProducts />
       </section>
     </main>
