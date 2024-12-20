27ddda358e4c88b920fa95740ec54390d2beff1d
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-12-20 22:55:54 +0200

    Update ProductCard component

--- src/components/GoBack/GoBack.jsx
+++ src/components/GoBack/GoBack.jsx
@@ -1,15 +1,15 @@
 import { HiArrowLeft } from 'react-icons/hi';
 import { Link } from 'react-router-dom';
+import css from './GoBack.module.css';
 
 const GoBack = ({ to, children }) => {
   return (
-    <div>
-      <Link to={to}>
-        <HiArrowLeft size="24" />
+    <div className={css.goBackWrapper}>
+      <Link to={to} className={css.goBackBtn}>
+        <HiArrowLeft size="24" className={css.icon} />
         {children}
       </Link>
     </div>
   );
 };
-
 export default GoBack;
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,12 +1,64 @@
+import css from './ProductCard.module.css';
+
 const ProductCard = ({ product }) => {
-  const { title, description, price, thumbnail } = product;
+  const {
+    title,
+    description,
+    price,
+    thumbnail,
+    category,
+    brand,
+    rating,
+    stock,
+    reviews,
+    shippingInformation,
+  } = product;
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className={css.card}>
       <img src={thumbnail} alt={title} />
-      <p>{description}</p>
-      <p>{price} $</p>
+
+      <div className={css.infoWrapper}>
+        <h1>{title}</h1>
+        <p>
+          <strong>Category:</strong> {category}
+        </p>
+        <p>
+          <strong>Brand:</strong> {brand}
+        </p>
+        <p>{description}</p>
+        <p>{price} $</p>
+        <p>
+          <strong>Rating:</strong> {rating} / 5
+        </p>
+        <p>
+          <strong>In Stock:</strong> {stock}
+        </p>
+        <p>
+          <strong>Shipping Information :</strong> {shippingInformation}
+        </p>
+
+        <div className={css.reviews}>
+          <h2>Customer Reviews</h2>
+          {reviews && reviews.length > 0 ? (
+            <ul>
+              {reviews.map((review, index) => (
+                <li key={index} className={css.reviewItem}>
+                  <p>
+                    {review.reviewerName}: {review.text}
+                  </p>
+                  <p>
+                    {review.comment}: {review.text}
+                  </p>
+                  <p>Rating: {review.rating} / 5</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No reviews yet.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
